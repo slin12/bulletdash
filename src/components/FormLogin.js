cc146b5ea6672690c4ef1d54882be0e8e768b8e6
Shirley <s.lin12@gmail.com>
2018-01-02 10:47:55 -0500

    add fade in to login/signup

--- src/components/FormLogin.js
+++ src/components/FormLogin.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { CSSTransitionGroup } from "react-transition-group";
 
 class FormLogin extends React.Component {
   state = {
@@ -28,38 +29,46 @@ class FormLogin extends React.Component {
     return (
       <div className="container">
         <h1 id="title">bulletdash</h1>
-        <div id="login">
-          <h2>login</h2>
-          <form onSubmit={this.handleSubmit}>
-            <fieldset>
-              <label className="form" htmlFor="nameField">
-                Username
-              </label>
-              <input
-                type="text"
-                id="nameField"
-                value={this.state.username}
-                onChange={e => this.handleChange(e.target.value, "username")}
-              />
-              <label className="form" htmlFor="password">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                value={this.state.password}
-                onChange={e => this.handleChange(e.target.value, "password")}
-              />
-              <Link to="/signup">
-                <small>Not Signed Up?</small>
-              </Link>
-              <br />
-              <button className="form-button" type="submit">
-                Login
-              </button>
-            </fieldset>
-          </form>
-        </div>
+        <CSSTransitionGroup
+          transitionName="login"
+          transitionAppear={true}
+          transitionAppearTimeout={600}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <div id="login" key="login-div">
+            <h2>login</h2>
+            <form onSubmit={this.handleSubmit}>
+              <fieldset>
+                <label className="form" htmlFor="nameField">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  id="nameField"
+                  value={this.state.username}
+                  onChange={e => this.handleChange(e.target.value, "username")}
+                />
+                <label className="form" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  value={this.state.password}
+                  onChange={e => this.handleChange(e.target.value, "password")}
+                />
+                <Link to="/signup">
+                  <small>Not Signed Up?</small>
+                </Link>
+                <br />
+                <button className="form-button" type="submit">
+                  Login
+                </button>
+              </fieldset>
+            </form>
+          </div>
+        </CSSTransitionGroup>
       </div>
     );
   }
--- src/components/FormSignup.js
+++ src/components/FormSignup.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { CSSTransitionGroup } from "react-transition-group";
 
 class FormSignup extends React.Component {
   state = {
@@ -43,59 +44,67 @@ class FormSignup extends React.Component {
         <h1 id="title">bulletdash</h1>
         <div id="signup">
           <h2>signup</h2>
-          <form onSubmit={this.handleSubmit}>
-            <fieldset>
-              <label className="form" htmlFor="nameField">
-                Username
-              </label>
-              <input
-                type="text"
-                id="nameField"
-                value={this.state.usernameValue}
-                onChange={e => this.handleChange(e, "usernameValue")}
-              />
-              <label className="form" htmlFor="password">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                value={this.state.passwordValue}
-                onChange={e => this.handleChange(e, "passwordValue")}
-              />
-              <label className="form" htmlFor="password">
-                Confirm Password{" "}
-                {this.state.passwordValue ===
-                this.state.confirmPasswordValue ? (
-                  <i className="success icon-ok-sign" />
-                ) : (
-                  <i className="no-success icon-remove-sign" />
-                )}
-              </label>
-              <input
-                type="password"
-                id="confirm-password"
-                value={this.state.confirmPasswordValue}
-                onChange={e => this.handleChange(e, "confirmPasswordValue")}
-              />
-              <label className="form" htmlFor="firstName">
-                First Name
-              </label>
-              <input
-                type="text"
-                id="firstName"
-                value={this.state.firstNameValue}
-                onChange={e => this.handleChange(e, "firstNameValue")}
-              />
-              <Link to="/login">
-                <small>Already Signed Up?</small>
-              </Link>
-              <br />
-              <button className="form-button" type="submit">
-                Signup
-              </button>
-            </fieldset>
-          </form>
+          <CSSTransitionGroup
+            transitionName="login"
+            transitionAppear={true}
+            transitionAppearTimeout={600}
+            transitionEnter={false}
+            transitionLeave={false}
+          >
+            <form onSubmit={this.handleSubmit}>
+              <fieldset>
+                <label className="form" htmlFor="nameField">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  id="nameField"
+                  value={this.state.usernameValue}
+                  onChange={e => this.handleChange(e, "usernameValue")}
+                />
+                <label className="form" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  value={this.state.passwordValue}
+                  onChange={e => this.handleChange(e, "passwordValue")}
+                />
+                <label className="form" htmlFor="password">
+                  Confirm Password{" "}
+                  {this.state.passwordValue ===
+                  this.state.confirmPasswordValue ? (
+                    <i className="success icon-ok-sign" />
+                  ) : (
+                    <i className="no-success icon-remove-sign" />
+                  )}
+                </label>
+                <input
+                  type="password"
+                  id="confirm-password"
+                  value={this.state.confirmPasswordValue}
+                  onChange={e => this.handleChange(e, "confirmPasswordValue")}
+                />
+                <label className="form" htmlFor="firstName">
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  id="firstName"
+                  value={this.state.firstNameValue}
+                  onChange={e => this.handleChange(e, "firstNameValue")}
+                />
+                <Link to="/login">
+                  <small>Already Signed Up?</small>
+                </Link>
+                <br />
+                <button className="form-button" type="submit">
+                  Signup
+                </button>
+              </fieldset>
+            </form>
+          </CSSTransitionGroup>
         </div>
       </div>
     );
